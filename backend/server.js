69327482866e2a57918a51f610c19a22de6bc2e0
LaurenceWiles <140092148+LaurenceWiles@users.noreply.github.com>
2024-05-07 14:46:09 +0200

    connected to mongodb and set up test route

---
+++ backend/server.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const cities = require("./routes/cities");
+const app = express();
+const router = express.Router();
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.json({ msg: "Welcome to the app" });
+});
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log("connected to db and listening on port", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+app.use("/all", cities);
