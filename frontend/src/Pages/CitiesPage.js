4ed05d696fded170ec45e7ad3a3b32a57cf151b1
LaurenceWiles <140092148+LaurenceWiles@users.noreply.github.com>
2024-05-10 13:30:11 +0200

    rendered cities list in frontend

--- frontend/src/Pages/CitiesPage.js
+++ frontend/src/Pages/CitiesPage.js
@@ -1,10 +1,24 @@
 import React from "react";
-import Footer from "../components/Footer";
+import { useEffect, useState } from "react";
 
 const CitiesPage = () => {
+  const [cities, setCities] = useState(null);
+
+  useEffect(() => {
+    const fetchCities = async () => {
+      const response = await fetch("http://localhost:4000/cities/all");
+      const json = await response.json();
+
+      if (response.ok) {
+        setCities(json);
+      }
+    };
+    fetchCities();
+  }, []);
+
   return (
     <div>
-      <h1>Cities Page</h1>
+      {cities && cities.map((city) => <p key={city._id}>{city.name}</p>)}
     </div>
   );
 };
