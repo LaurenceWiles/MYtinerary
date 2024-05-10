186d04eb5044107f6906f875c5385a63837bcf7d
LaurenceWiles <140092148+LaurenceWiles@users.noreply.github.com>
2024-05-10 10:40:11 +0200

    created get all cities route

--- backend/models/cityModel.js
+++ backend/models/cityModel.js
@@ -1,11 +1,18 @@
 const mongoose = require("mongoose");
 
-const citySchema = new mongoose.Schema({
+const cityModel = new mongoose.Schema({
   name: {
     type: String,
     required: true,
     unique: true,
   },
+  country: {
+    type: String,
+    required: true,
+  },
+  img: {
+    type: String,
+  },
 });
 
-module.exports = mongoose.model("cities", citySchema);
+module.exports = mongoose.model("cities", cityModel);
--- backend/routes/cities.js
+++ backend/routes/cities.js
@@ -4,10 +4,12 @@ const router = express.Router();
 
 const cityModel = require("../models/cityModel");
 
+//test route
 router.get("/", (req, res) => {
   res.send({ msg: "Cities test route." });
 });
 
+//get all cities
 router.get("/all", (req, res) => {
   cityModel
     .find({})
